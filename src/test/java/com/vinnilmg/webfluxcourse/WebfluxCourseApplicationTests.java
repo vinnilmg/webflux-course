954497a0e9e74cef8e44257d92d41fdf113e390f
vinnilmg <vini.delimagomes@gmail.com>
2023-11-28 20:20:16 -0300

    init app

---
+++ src/test/java/com/vinnilmg/webfluxcourse/WebfluxCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinnilmg.webfluxcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
