3ca473e0168f5dbb0ea10022ce3833ad5fb3bdda
vinnilmg <vini.delimagomes@gmail.com>
2023-12-03 19:58:23 -0300

    add user entity

---
+++ src/main/java/com/vinnilmg/webfluxcourse/entity/User.java
@@ -0,0 +1,21 @@
+package com.vinnilmg.webfluxcourse.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+public class User {
+
+    @Id
+    private String id;
+    private String name;
+
+    @Indexed(unique = true)
+    private String email;
+    private String password;
+}
