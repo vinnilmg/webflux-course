b35b276a99c0ff1b5199e86248b209f0e86e4368
vinnilmg <vini.delimagomes@gmail.com>
2023-12-07 21:29:49 -0300

    feat: adicionando validacoes de campo + exception handler para validation error

--- src/main/java/com/vinnilmg/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/vinnilmg/webfluxcourse/controller/exceptions/ControllerExceptionHandler.java
@@ -3,8 +3,10 @@ package com.vinnilmg.webfluxcourse.controller.exceptions;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
 import reactor.core.publisher.Mono;
 
 import static java.time.LocalDateTime.now;
@@ -27,6 +29,24 @@ public class ControllerExceptionHandler {
                                 .path(request.getPath().toString())
                                 .build()
                 ));
+    }
+
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<Mono<ValidationError>> validationError(
+            WebExchangeBindException e, ServerHttpRequest request
+    ) {
+        ValidationError error = new ValidationError(
+                now(),
+                request.getPath().toString(),
+                BAD_REQUEST.value(),
+                "Validation Error",
+                "Error on validation attributes"
+        );
+
+        // Pega field errors
+        e.getBindingResult().getFieldErrors().forEach(fe -> error.addError(fe.getField(), fe.getDefaultMessage()));
+
+        return ResponseEntity.badRequest().body(Mono.just(error));
     }
 
     private String verifyDupKey(String message) {
--- src/main/java/com/vinnilmg/webfluxcourse/model/request/UserRequest.java
+++ src/main/java/com/vinnilmg/webfluxcourse/model/request/UserRequest.java
@@ -1,7 +1,20 @@
 package com.vinnilmg.webfluxcourse.model.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record UserRequest(
+
+        @Size(min = 3, max = 50, message = "must be between 3 and 50 characters")
+        @NotBlank(message = "must not be null or empty")
         String name,
+
+        @Email(message = "invalid e-mail")
+        @NotBlank(message = "must not be null or empty")
         String email,
+
+        @Size(min = 3, max = 20, message = "must be between 3 and 20 characters")
+        @NotBlank(message = "must not be null or empty")
         String password
 ) { }
