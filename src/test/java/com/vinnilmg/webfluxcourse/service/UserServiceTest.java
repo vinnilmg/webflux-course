8fcec89e26feb869703f0fa3f477bd3afeda38d9
vinnilmg <vini.delimagomes@gmail.com>
2023-12-12 19:37:59 -0300

    feat: add updateuser unittest

--- src/test/java/com/vinnilmg/webfluxcourse/service/UserServiceTest.java
+++ src/test/java/com/vinnilmg/webfluxcourse/service/UserServiceTest.java
@@ -76,4 +76,28 @@ class UserServiceTest {
 
         verify(repository, times(1)).findAll();
     }
+
+    @Test
+    void testUpdate() {
+        var request = new UserRequest("Vini", "vini@mail.com", "my-passw-test");
+        var entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class), any(User.class))).thenReturn(entity);
+        when(repository.findById(anyString())).thenReturn(Mono.just(entity));
+        when(repository.save(any(User.class))).thenReturn(Mono.just(entity));
+
+        Mono<User> result = service.update("123", request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        verify(repository, times(1)).findById(anyString());
+        verify(repository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    void testDelete() {
+    }
 }
