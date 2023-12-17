d6fd47554a989dd0dab4c67a82a258cec8a7cf1a
vinnilmg <vini.delimagomes@gmail.com>
2023-12-17 20:49:45 -0300

    feat: teste de integração para o endpoint save (sucesso)

---
+++ src/test/java/com/vinnilmg/webfluxcourse/controller/UserControllerImplTest.java
@@ -0,0 +1,74 @@
+package com.vinnilmg.webfluxcourse.controller;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.vinnilmg.webfluxcourse.entity.User;
+import com.vinnilmg.webfluxcourse.mapper.UserMapper;
+import com.vinnilmg.webfluxcourse.model.request.UserRequest;
+import com.vinnilmg.webfluxcourse.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.BodyInserters;
+import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UserControllerImplTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private UserService service;
+
+    @MockBean
+    private UserMapper mapper;
+
+    @MockBean
+    private MongoClient mongoClient;
+
+    @Test
+    @DisplayName("Test endpoint save with success")
+    void testSaveWithSuccess() {
+        final var request = new UserRequest("Mariazinha", "maria@mail.com", "password123");
+
+        when(service.save(any(UserRequest.class))).thenReturn(Mono.just(User.builder().build()));
+
+        webTestClient.post()
+                .uri("/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(request))
+                .exchange()
+                .expectStatus().isCreated();
+
+        verify(service).save(any(UserRequest.class)); // Default: times == 1
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
